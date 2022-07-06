9dc3c1ca5fd3ce613681b25e2593540a36795600
hxlsps <xuanloc11042000@gmail.com>
2022-07-06 21:20:37 +0700

    Bai tap tren lop\

--- LESSON 3/string_practice.py
+++ LESSON 3/string_practice.py
@@ -11,7 +11,7 @@ print(len(str_txt))
 #5. Check if string str_txt contains another string. If has, print start position of that substring.
 get_check=input('Enter string: ')
 if get_check in str_txt:
-    print(str_txt.find(get_check))
+    print(str_txt.index(get_check))
 else:
     print('No substring')    
 #6. Print str with all the characters are capitalized/ normalized
@@ -23,4 +23,5 @@ print(str_txt.replace(' ',''))
 #8. Replace all the h character with m character
 print(str_txt.replace('h','m'))
 #9. Split str_txt by white spaces and display second item of array
-print(str_txt.split(' '))
+pt=str_txt.split(' ')
+print(pt[1])
